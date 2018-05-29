4cff0320c8053db4ada6ad044d1c70d337ccc6c7
mr-kaveh <mr.hdavoodi@gmail.com>
2018-05-29 07:54:20 +0430

    Operation Commit

---
+++ Operation/alerts/alertSetup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Disk Alert
+kapacitor define disk_usage_alert -tick alerts/disk/disk_usage_percent.tick -dbrp "telegraf"."autogen" && kapacitor enable disk_usage_alert;
+
+#Cpu Alerts
+kapacitor define cpu_idle -tick alerts/cpu/cpu_idle.tick -dbrp "telegraf"."autogen" && kapacitor enable cpu_idle;
+kapacitor define system_usage -tick alerts/cpu/system_usage.tick -dbrp "telegraf"."autogen" && kapacitor enable system_usage;
+kapacitor define user_usage -tick alerts/cpu/user_usage.tick -dbrp "telegraf"."autogen" && kapacitor enable user_usage;
+
+#Memory Alert
+kapacitor define memory_usage -tick alerts/memory/memory_usage.tick -dbrp "telegraf"."autogen" && kapacitor enable memory_usage;
+
+#System Alerts
+kapacitor define load15_alert -tick alerts/system/load15.tick -dbrp "telegraf"."autogen" && kapacitor enable load15_alert;
+kapacitor define load5_alert -tick alerts/system/load5.tick -dbrp "telegraf"."autogen" && kapacitor enable load5_alert;
+kapacitor define load1_alert -tick alerts/system/load1.tick -dbrp "telegraf"."autogen" && kapacitor enable load1_alert;
